3ae13f8cbf4f33ba5187fdcd36860b37765f7d94
Gozkybrain <gozkybrain@gmail.com>
2025-10-08 10:38:40 +0100

    minor updates

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -6,29 +6,29 @@ import styles from "../styles/Loader.module.css";
 export default function Loader() {
   return (
     <div className={styles.loaderWrapper}>
-      {/* Curved Text */}
-      <svg
-        viewBox="0 0 300 150"
-        className={styles.curvedText}
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <defs>
-          <path
-            id="curve"
-            d="M 50 120 A 100 100 0 0 1 250 120"
-            fill="transparent"
-          />
-        </defs>
-        <text className={styles.loaderText}>
-          <textPath href="#curve" startOffset="50%" textAnchor="middle">
-            TRIVIB
-          </textPath>
-        </text>
-      </svg>
+      <div className={styles.loaderBox}>
+        {/* Curved Text */}
+        <svg
+          viewBox="0 0 300 150"
+          className={styles.curvedText}
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <path
+              id="curve"
+              d="M 50 120 A 100 100 0 0 1 250 120"
+              fill="transparent"
+            />
+          </defs>
+          <text className={styles.loaderText}>
+            <textPath href="#curve" startOffset="50%" textAnchor="middle">
+              TRIVIB
+            </textPath>
+          </text>
+        </svg>
 
-      {/* Alien Icon */}
-      <div className={styles.alienWrapper}>
-        👽
+        {/* Alien Icon */}
+        <div className={styles.alienWrapper}>👽</div>
       </div>
     </div>
   );
